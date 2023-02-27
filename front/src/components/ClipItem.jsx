1b54a39e337077ada0aa01c3b6b941d7149183a9
Ubuntu <ubuntu@ip-172-31-12-193.eu-central-1.compute.internal>
2023-02-27 17:03:54 +0000

    Fixes

--- front/src/components/ClipItem.jsx
+++ front/src/components/ClipItem.jsx
@@ -59,18 +59,24 @@ const ClipItem = (props) => {
                         <option value="East Asian">East Asian</option>
                         <option value="Caucasian Latin">Caucasian Latin</option>
                         <option value="Asian Indian">Asian Indian</option>
+                        <option value="Arab">Arab</option>
                     </select>
+                    <input hidden value={props.post.subclipId} name="clipid"></input>
                     <MyButton disabled={isDisabled}
                               onClick={() => {
+                                  selectOkValues.videoId = props.post.subclipId;
                                   ClipService.putOkClips({ ...selectOkValues});
-                                  props.remove(props.post)}}
+                                  props.remove(props.post);
+                                    
+                                }}
                                   style={{marginLeft: 10}}>
                                      Ok
                     </MyButton>
-                    <MyButton onClick={() => {
+                    <MyButton  onClick={() => {
+                        selectNotOkValues.videoId = props.post.subclipId;
                         ClipService.putNotOkClips({ ...selectNotOkValues});
                         props.remove(props.post)}}
-                              style={{marginLeft: 10}}>
+                              style={{marginLeft: 10, color: 'white',  backgroundColor: 'indianred'}}>
                                      Not Ok
                     </MyButton>
                 </div>
