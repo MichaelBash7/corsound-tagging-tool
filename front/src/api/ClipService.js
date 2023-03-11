284fc0c1839b2fb26eca5de86dbecbaf5a46e36d
st <s.chirkin@mail1.pro>
2023-03-11 20:41:45 +0300

    User table / show statistics

--- front/src/api/ClipService.js
+++ front/src/api/ClipService.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 
 export default class ClipService {
+    static async getUserStat(login) {
+        if (!login) return
+        return await axios.get(`http://54.246.41.115/videos/get_stats/?dataset=bc_queries&reviewer_email=${login}&token=11`, {
+            headers: {
+                'accept': 'application/json',
+                'x-token': '11'
+            }
+        })
+    }
+
     static async getAllClips(limit = 5, page = 1) {
         return await axios.get('http://54.246.41.115/videos/subclips_all/?dataset=bc_queries&limit=100&token=11', {
             // params: {
--- front/src/components/UserTable.tsx
+++ front/src/components/UserTable.tsx
@@ -1,15 +1,33 @@
-import React, {FC, useContext, useState} from 'react';
+import React, {FC, useContext, useState, useEffect} from 'react';
 import {Context} from "../index";
-import {Table, Popconfirm} from "antd";
+import {Table, Popconfirm, Space, Button, Typography} from "antd";
 import {observer} from "mobx-react-lite";
 import {DeleteOutlined, EditTwoTone} from '@ant-design/icons';
+import ClipService from "../api/ClipService";
 
 const UserTable : FC = () => {
 
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
     const {store} = useContext(Context)
+    const { Text } = Typography;
 
+    type User = {
+        id: number,
+        login: string,
+        data: object,
+    }
+    const dataSource = [
+        {
+            id: 1,
+            login: 'arkady.krishtul@corsound.ai',
+            data: {},
+        },
+        {
+            id: 2,
+            login: 'Mihaelbash@yandex.ru',
+            data: {},
+        },
+    ]
+    const [users, setUsers] = useState<Array<User>>(dataSource)
     const handleDelete = (id: number) => {
         console.log(id)
     }
@@ -17,26 +35,31 @@ const UserTable : FC = () => {
         console.log(id)
     }
 
-    const dataSource = [
-        {
-            id: '1',
-            login: 'Mike',
-        },
-        {
-            id: '2',
-            login: 'John',
+    const renderDataCell = (data: any) => {
+        console.log(data.reviewed)
+        let list = []
+        for (const [k, v] of Object.entries(data)) {
+            list.push(
+                    <Text><>{k}: {v}</></Text>
+            )
         }
-    ];
+            return (
+                <Space direction="vertical">
+                    {list}
+                </Space>
+            )
+
+    }
 
     const columns = [
-        {
-            title: 'Edit',
-            key: 'edit',
-            render: (_: any, record: any) => (
-                <EditTwoTone value={record.id} />
-            ),
-            width: 50,
-        },
+        // {
+        //     title: 'Edit',
+        //     key: 'edit',
+        //     render: (_: any, record: any) => (
+        //         <EditTwoTone value={record.id} />
+        //     ),
+        //     width: 50,
+        // },
         {
             title: 'ID',
             dataIndex: 'id',
@@ -49,19 +72,50 @@ const UserTable : FC = () => {
             key: 'login',
         },
         {
-            title: 'Del',
-            key: 'delete',
-            render: (_: any, record: any) => (
-                <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.id)}>
-                    <DeleteOutlined />
-                </Popconfirm>
-            ),
-            width: 50,
+            title: 'Data',
+            dataIndex: 'data',
+            key: 'data',
+            render: (object: object) => renderDataCell(object)
         },
+        // {
+        //     title: 'Del',
+        //     key: 'delete',
+        //     render: (_: any, record: any) => (
+        //         <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.id)}>
+        //             <DeleteOutlined />
+        //         </Popconfirm>
+        //     ),
+        //     width: 50,
+        // },
     ];
+    const addData = () => {
+        const newUsers = users
+        newUsers.map((user, index) => {
+        //    if (Object.keys(user.data).length == 0) {
+                 ClipService.getUserStat(user.login).then((res) => {
+                    if (res) {
+                        newUsers[index].data = res.data.reviewers[user.login]
+                        setUsers([...newUsers])
+                        console.log(newUsers)
+                        //    user.data = res.data.reviewers[user.login]['reviewed']
+                    }
+                 }).catch((e) => `Error: ${e}`)
+      //      }
+        })
+    }
 
     return (
-        <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 50 }} />
+        <>
+         <Space wrap>
+            <Button type="primary" onClick={() => addData()}>Load statistics</Button>
+        </Space>
+        <Table
+            dataSource={users}
+            columns={columns}
+            pagination={{ pageSize: 50 }}
+            rowKey='id'
+        />
+        </>
     );
 };
 
